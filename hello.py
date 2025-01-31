9f8ee69b872e3b351d7e3ece10811bd062375a2a
ashdsabandal <adsabandal@up.edu.ph>
2025-01-31 13:59:04 +0800

    MODIFIED: hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,31 @@
 print("hello world")
+print("i love cmsc 23 <333")
+
+"""
+PS C:\Users\User\Desktop\sabandal> git init
+Initialized empty Git repository in C:/Users/User/Desktop/sabandal/.git/
+PS C:\Users\User\Desktop\sabandal> git config --global --unset user.name
+PS C:\Users\User\Desktop\sabandal> git config --global --unset user.email
+PS C:\Users\User\Desktop\sabandal> git config --local user.name ashdsabandal
+PS C:\Users\User\Desktop\sabandal> git config --local user.email adsabandal@up.edu.ph
+PS C:\Users\User\Desktop\sabandal> git status
+On branch master
+
+No commits yet
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        hello.py
+
+nothing added to commit but untracked files present (use "git add" to track)
+PS C:\Users\User\Desktop\sabandal> git add -A
+PS C:\Users\User\Desktop\sabandal>  git status
+On branch master
+
+No commits yet
+
+Changes to be committed:
+  (use "git rm --cached <file>..." to unstage)
+        new file:   hello.py
+
+"""
